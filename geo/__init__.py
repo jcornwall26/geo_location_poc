3cc16460c798773a5143e2911b4cc8f764864e1e
jcornwall26 <jasoncornwall@lululemon.com>
2021-02-09 16:55:59 -0800

    initial commit

---
+++ geo/__init__.py
@@ -0,0 +1,4 @@
+from geo.factories import create_geo_coded_store
+from geo.geo_store import GeoStore
+
+__all__ = ["echo", "surround", "reverse"]
---
+++ geo/geo_store.py
@@ -0,0 +1,45 @@
+class GeoStore:
+
+    def __init__(self, store, geo_result):
+        self._store = store
+        self._number = None
+
+        if geo_result is None:
+            print(f'geo_result is None')
+
+        self.geo_result = geo_result
+        super().__init__()
+    
+    @property
+    def store(self) -> str:
+        return self._store
+
+    @property
+    def number(self) -> str:
+        if self._number is not None:
+            return self._number
+        return self.geo_result['number']
+        
+    @number.setter
+    def number(self, value) -> str:
+        self._number = value
+
+    @property
+    def street(self) -> str:
+        return self.geo_result['street']
+
+    @property
+    def locality(self):
+        return self.geo_result['locality']
+
+    @property
+    def country(self) -> str:
+        return self.geo_result['country']
+
+    @property
+    def latitude(self) -> str:
+        return self.geo_result['latitude']
+
+    @property
+    def longitude(self) -> str:
+        return self.geo_result['longitude']
